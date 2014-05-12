84dc872cc65318c43457af2bd4296fc44935330f
Sergei Orlov <sorlov+github@gmail.com>
2014-05-12 09:22:53 -0400

    WIP

--- art.clj
+++ art.clj
@@ -3,26 +3,26 @@
            (java.awt Graphics Dimension Color)
            (java.awt.image BufferedImage)))
 
-(def MAX-POINTS 10)
-(def SIZE 400)
+(def MAX-POINTS 40)
+(def SIZE 600)
+
+(defn draw-circle [gfx x y radius]
+  (.drawOval gfx (- x radius) (- y radius) (* 2 radius) (* 2 radius)))
 
 (defn paint-canvas [graphics size closest-point-fn]
   (doseq [y (range size)
           x (range size)]
-
     (let [[_ _ r g b] (closest-point-fn x y)]
       ; can maybe save some time if I store the color object instead of
       ; creating it for every point anew
       (.setColor graphics (Color. r g b))
       (.drawLine graphics x y x y))))
 
-(defn draw [size closest-point-fn]
+(defn draw [& {:keys [size closest-point-fn]
+               :or {size 250 closest-point-fn (fn [x y] [3 14 0 255 128])}}]
   (let [image (BufferedImage. size size BufferedImage/TYPE_INT_RGB)
-        canvas (proxy [JLabel] []
-                 (paint [g] (.drawImage g image 0 0 this)))]
-
+        canvas (proxy [JLabel] [] (paint [g] (.drawImage g image 0 0 this)))]
     (paint-canvas (.createGraphics image) size closest-point-fn)
-
     (doto (JFrame.)
       (.add canvas)
       (.setSize (Dimension. size size))
@@ -39,7 +39,7 @@
 
 (defn divisible-by-fn [by-what]
   "Curry function that will test if a number is divisible by-something."
-  (fn [number] (let [quotient (/ (int number) by-what)]
+  (fn [number] (let [quotient (/ (int (mod number 1000000)) by-what)]
                  (= (int quotient) quotient))))
 
 (defn square [x] (* x x))
@@ -52,201 +52,142 @@
 (defn abs [x]
   (if (>= x 0) x (- x)))
 
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;
 ; distance transformators
 ;;;;;;;;;;;;;;;;;;;;;;;;;
 
-; TODO: make transformers composable
+(defn compose [f & fs]
+  (if (empty? fs) f
+    (let [second-func-curried (partial (first fs) f)]
+      (apply compose (cons second-func-curried (rest fs))))))
 
-(defn blur [f x1 y1 x2 y2]
+(defn get-closest-point-fn [f-distance set-points]
+  "Turns a distance function into a get-closest-point function."
+  (fn [x1 y1]
+    (let [distance-to-set-point (fn [[x2 y2 _ _ _]] (f-distance x1 y1 x2 y2))]
+      (min-by distance-to-set-point set-points))))
+
+(defn noise-blur [f x1 y1 x2 y2]
   "Takes any distance function and essentially adds noise"
   (if (zero? (rand-int 2))
     (* 2 (f x1 y1 x2 y2))
     (f x1 y1 x2 y2)))
-
-(defn pointy [f x1 y1 x2 y2]
-  "Usage: (pointy distance-average x1 y1 x2 y2)"
-  (if (even? (+ x2 y2))
-    (* 2 (f x1 y1 x2 y2))
-    (f x1 y1 x2 y2)))
-
-(defn sum-scanline [f x1 y1 x2 y2]
-  "Usage: (sum-scanline distance-average x1 y1 x2 y2)"
-  (if (even? (+ x1 y1 x2 y2))
-    (* 2 (f x1 y1 x2 y2))
-    (f x1 y1 x2 y2)))
-
-(defn overlap [f x1 y1 x2 y2]
-  "Usage: (overlap distance-average x1 y1 x2 y2)"
-  (if (even? (bit-xor x1 y1 x2 y2))
-    (* 100 (f x1 y1 x2 y2))
-    (f x1 y1 x2 y2)))
-
-(defn xor-scanline [f x1 y1 x2 y2]
-  "Usage: (xor-scanline distance-average x1 y1 x2 y2)"
-  (if (even? (bit-xor x1 y1 x2 y2))
-    (* 2 (f x1 y1 x2 y2))
-    (f x1 y1 x2 y2)))
-
-(defn mult-scanline [f x1 y1 x2 y2]
-  "Usage: (mult-scanline distance-average x1 y1 x2 y2)"
-  (if (even? (* x1 y1 x2 y2))
-    (* 2 (f x1 y1 x2 y2))
-    (f x1 y1 x2 y2)))
-
-(defn scanline [f-shape scanline-width]
-  (fn [f-dist x1 y1 x2 y2]
-    (if ((divisible-by-fn scanline-width) (f-shape x1 y1 x2 y2))
-      (* 1000 (f-dist x1 y1 x2 y2))
-      (f-dist x1 y1 x2 y2))))
-
-(defn abstract-scanline [f-scanline-dist f-scanline-shape f-dist x1 y1 x2 y2]
-  "Usage: (abstract-scanline distance-evener distance-average x1 y1 x2 y2)"
-        (if (f-scanline-shape (f-scanline-dist x1 y1 x2 y2))
-          (* 1000 (f-dist x1 y1 x2 y2))
-          (f-dist x1 y1 x2 y2)))
 
 
 ;;;;;;;;;;;;;;;;;;;
 ;distance functions
 ;;;;;;;;;;;;;;;;;;;
 
-(defn distance-evener [x1 y1 x2 y2]
+(defn d-evener [x1 y1 x2 y2]
   (let [average (/ (+ (abs (- x2 x1)) (abs (- y2 y1))) 2)]
     (if (even? (+ x1 y1 x2 y2)) (* 2 average) average)))
 
-(defn distance-average [x1 y1 x2 y2]
+(defn d-avg [x1 y1 x2 y2]
   (/ (+ (abs (- x2 x1)) (abs (- y2 y1))) 2))
 
-(defn distance-average-mixmatch [x1 y1 x2 y2]
+(defn d-avg-mismatch [x1 y1 x2 y2]
   (/ (+ (abs (- x2 y1)) (abs (- y2 x1))) 2))
 
-(defn distance-average-random-negative [x1 y1 x2 y2]
+(defn d-avg-rand-neg [x1 y1 x2 y2]
   (let [average (/ (+ (abs (- x2 x1)) (abs (- y2 y1))) 2)]
     (if (zero? (rand-int 2)) (- average) average)))
 
-(defn distance-average-random-double [x1 y1 x2 y2]
+(defn d-avg-rand-dbl [x1 y1 x2 y2]
   (let [average (/ (+ (abs (- x2 x1)) (abs (- y2 y1))) 2)]
     (if (zero? (rand-int 2)) (* 2 average) average)))
 
-(defn distance-average-random-triple [x1 y1 x2 y2]
-  (let [average (/ (+ (abs (- x2 x1)) (abs (- y2 y1))) 2)]
-    (if (zero? (rand-int 2)) (* 3 average) average)))
-
-(defn distance-linear-reversed [x1 y1 x2 y2]
-  (- (+ (abs (- x2 x1)) (abs (- y2 y1)))))
-
-(defn distance-linear [x1 y1 x2 y2]
+(defn d-linear [x1 y1 x2 y2]
+  "Sum of the distance in the x direction and the y"
   (+ (abs (- x2 x1)) (abs (- y2 y1))))
 
-(defn distance-euclidean [x1 y1 x2 y2]
+(defn d-linear-r [x1 y1 x2 y2]
+  "-1 * Sum of the distance in the x direction and the y"
+  (- (+ (abs (- x2 x1)) (abs (- y2 y1)))))
+
+(defn d-euclidean [x1 y1 x2 y2]
+  "Euclidean Distance"
   (Math/sqrt (+ (square (- x2 x1)) (square (- y2 y1)))))
 
-(defn distance-euclidean-reversed [x1 y1 x2 y2]
+(defn d-euclidean-r [x1 y1 x2 y2]
+  "Negative euclidean distance"
   (- (Math/sqrt (+ (square (- x2 x1)) (square (- y2 y1))))))
 
-(defn distance-max-dimension [x1 y1 x2 y2]
+(defn d-max [x1 y1 x2 y2]
+  "Distance that is the maximum of the distance in the x direction, and the y"
   (let [xdist (abs (- x2 x1)) ydist (abs (- y2 y1))]
     (max xdist ydist)))
 
-(defn distance-min-dimension [x1 y1 x2 y2]
+(defn d-min [x1 y1 x2 y2]
+  "Distance that is the minimum of the distance in the x direction, and the y"
   (let [xdist (abs (- x2 x1)) ydist (abs (- y2 y1))]
     (min xdist ydist)))
 
-(defn distance-average-dimension [x1 y1 x2 y2]
+(defn d-avg-dimension [x1 y1 x2 y2]
+  "Distance that averages the distance in the x direction and the y direction"
   (let [xdist (abs (- x2 x1)) ydist (abs (- y2 y1))]
     (/ (+ xdist ydist) 2)))
 
-(defn distance-min-max [x1 y1 x2 y2]
-  (let [min-dist (distance-min-dimension x1 y1 x2 y2)
-        max-dist (distance-max-dimension x1 y1 x2 y2)]
+(defn d-min-max [x1 y1 x2 y2]
+  "Distance that averages min-dist and max-dist."
+  (let [min-dist (d-min x1 y1 x2 y2)
+        max-dist (d-max x1 y1 x2 y2)]
     (/ (+ min-dist max-dist) 2)))
 
-(defn get-closest-point4 [x1 y1 set-points]
-  (let [distance-curry (fn [[x2 y2 _ _ _]] (abstract-scanline distance-average-random-double (fn [x] (even? (int x))) distance-max-dimension x1 y1 x2 y2))]
-    (min-by distance-curry set-points)))
+(defn scanline [f-shape & {:keys [width] :or {width 2}}]
+  "Make scanline function using f-shape for shape."
+  (fn [f-dist x1 y1 x2 y2]
+    (if ((divisible-by-fn width) (f-shape x1 y1 x2 y2))
+      Double/MAX_VALUE
+      (f-dist x1 y1 x2 y2))))
 
-; (defn get-closest-point [x1 y1 set-points]
-;   (let [distance-curry (fn [[x2 y2 _ _ _]] (abstract-scanline distance-euclidean-reversed even? distance-max-dimension x1 y1 x2 y2))]
-;     (min-by distance-curry set-points)))
+(defn scanline2 [f-shape & {:keys [width] :or {width 2}}]
+  "Make scanline function using f-shape for shape."
+  (fn [f-dist x1 y1 x2 y2]
+    (if ((divisible-by-fn width) (f-shape x1 y1 x2 y2))
+      (f-shape x1 y1 x2 y2)
+      (f-dist x1 y1 x2 y2))))
 
-; (defn get-closest-point [x1 y1 set-points]
-;   (let [distance-curry (fn [[x2 y2 _ _ _]] (abstract-scanline distance-linear-reversed even? distance-max-dimension x1 y1 x2 y2))]
-;     (min-by distance-curry set-points)))
 
+(def busy (compose d-min (scanline d-euclidean)))
+(def scanny (compose d-max (scanline d-euclidean)))
+(def noisy (compose d-min (scanline + :width 5) noise-blur))
 
-(defn get-closest-point3 [x1 y1 set-points]
-  (let [distance-curry (fn [[x2 y2 _ _ _]] (abstract-scanline distance-euclidean (fn [x] (even? (int x))) distance-max-dimension x1 y1 x2 y2))]
-    (min-by distance-curry set-points)))
+(def gcp1 (compose d-min (scanline d-min :width 5)))
+(def gcp2 (compose d-max (scanline d-linear-r :width 2)))
+(def gcp3 (compose d-max (scanline d-euclidean-r :width 4)))
+(def gcp4 (compose d-max (scanline d-euclidean-r)))
+(def gcp5 (compose d-max (scanline d-min :width 3)))
+(def gcp6 (compose d-max (scanline d-euclidean)))
+(def gcp7 (compose d-max (scanline d-avg-rand-dbl)))
+(def gcp8 (compose d-max (scanline d-min :width 3)))
 
-(defn get-closest-point2 [x1 y1 set-points]
-  (let [distance-curry (fn [[x2 y2 _ _ _]] (abstract-scanline
-                                             distance-min-dimension
-                                             (divisible-by-fn 3)
-                                             distance-max-dimension
-                                             x1 y1 x2 y2))]
-    (min-by distance-curry set-points)))
+(def d1 (compose d-min-max (scanline +)))
+(def d2 (compose d-min-max (scanline *)))
+(def d3 (compose d-min-max (scanline +) (scanline *)))
+(def d4 (compose d-min-max (scanline + :width 3) (scanline * :width 3)))
 
-; (defn get-closest-point2 [x1 y1 set-points]
-;   (let [distance-curry (fn [[x2 y2 _ _ _]] (abstract-scanline
-;                                              distance-euclidean-reversed
-;                                              (divisible-by-fn 4)
-;                                              distance-max-dimension
-;                                              x1 y1 x2 y2))]
-;     (min-by distance-curry set-points)))
-
-; (defn get-closest-point [x1 y1 set-points]
-;   (let [distance-curry (fn [[x2 y2 _ _ _]] (abstract-scanline
-;                                              distance-euclidean
-;                                              (divisible-by-fn 2)
-;                                              distance-max-dimension
-;                                              x1 y1 x2 y2))]
-;     (min-by distance-curry set-points)))
-
-(defn get-closest-point-original [x1 y1 set-points]
-  (let [distance-curry (fn [[x2 y2 _ _ _]] (abstract-scanline
-                                             distance-min-dimension
-                                             (divisible-by-fn 5)
-                                             distance-min-dimension
-                                             x1 y1 x2 y2))]
-    (min-by distance-curry set-points)))
-
-(defn get-closest-point-fn [f-distance set-points]
-  (fn [x1 y1]
-    (let [distance-to-set-point (fn [[x2 y2 _ _ _]] (f-distance x1 y1 x2 y2))]
-      (min-by distance-to-set-point set-points))))
-
-(defn distance4 [x1 y1 x2 y2]
-  (abstract-scanline distance-euclidean (divisible-by-fn 2)
-                     distance-max-dimension x1 y1 x2 y2))
-
-;(compose-distance eucliden (scanlines :width 4 :shape distance-min) blur)
-
-(defn compose [f & fs]
-  (if (empty? fs) f
-    (let [second-func-curried (fn [x1 y1 x2 y2] ((first fs) f x1 y1 x2 y2))]
-      (apply compose (cons second-func-curried (rest fs))))))
-
-(def d1 (compose distance-min-max sum-scanline))
-(def d2 (compose distance-min-max mult-scanline))
-(def d3 (compose distance-min-max sum-scanline mult-scanline))
-(def d4 (compose distance-min-max sum-scanline mult-scanline))
-
-(def gcp2 (compose distance-max-dimension (scanline distance-min-dimension 3)))
+(def t1 (compose d-min (scanline + :width 5) noise-blur))
+(def t2 (compose d-min noise-blur (scanline + :width 5)))
 
 (defn main []
-  (let [set-points (random-points MAX-POINTS SIZE SIZE)]
-    ; (draw SIZE (fn [x1 x2] (get-closest-point2 x1 x2 set-points)))
-    (draw SIZE (get-closest-point-fn gcp2 set-points))
-    ; (draw SIZE (get-closest-point-fn d1 set-points))
-    ; (draw SIZE (get-closest-point-fn d2 set-points))
-    ; (draw SIZE (get-closest-point-fn d3 set-points))
+  (let [set-points (random-points MAX-POINTS SIZE SIZE)
+        easy-draw (fn [f-dist]
+                    (draw :size SIZE
+                          :closest-point-fn (get-closest-point-fn f-dist set-points)
+                    ))]
+    (easy-draw d1)
+    (easy-draw d2)
+    (easy-draw d3)
+    (easy-draw d4)
 ))
+
 
 (main)
 
-;for each pair of points in set-points, draw a transparent(?) line, or
-;curve
+;What I really want is a framework that makes it very simple to do things.
+;for each point set color color-func
+;for each pair of points, connect by a line of color color-func, opacity 3
 ;
 ;Do this sort of personal-space simulation in real-time, with an led floor and
 ;either pressure pads in the floor or good computer vision / kinects
